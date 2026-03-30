fd73709b843675a5fcf89a2245a389940fbc6717
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2026-03-30 14:21:39 +0200

    Finished ex 2 (it's ugly)

--- src/main/java/it/unibo/pps/polyglot/a01b/Logics.java
+++ src/main/java/it/unibo/pps/polyglot/a01b/Logics.java
@@ -2,6 +2,29 @@ package it.unibo.pps.polyglot.a01b;
 import java.util.Optional;
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
+/**
+ * Scopo di questo esercizio è realizzare una GUI con l'aspetto mostrato nell'immagine fig.png, fornita,
+ * che realizza una mini-versione di Campo Minato. All'avvio si prepara un campo di gara quadrato
+ * (dimensione indicata dal primo parametro del costruttore), e si collocano in modo random le mine (in numero
+ * indicato nel secondo parametro) facendo attenzioni che siano in posizioni distinte
+ * (si stampi su console la loro posizione, a fini di debug).
+ * Alla pressione di un pulsante, questo si disabilita: se contiene una mina si esca indicando la sconfitta,
+ * altrimenti si mostri sul pulsante quante mine sono presenti in un vicino della cella clickata
+ * (in orizzontale, verticale o diagonale).
+ * Colpite tutte le celle che non hanno delle mine, si avrà vinto: si produca una stampa a console e si esca.
+ *
+ * Sono considerati opzionali ai fini della possibilità di correggere l'esercizio, ma concorrono comunque al raggiungimento
+ * della totalità del punteggio:
+ * - scorporamento di tutti gli aspetti che non sono di view in una interfaccia+classe esterna, via Strategy
+ * - compilazione e esecuzione dell'esercizio da linea di comando
+ *
+ * La classe GUIExample fornita include codice che potrebbe essere utile per la soluzione.
+ *
+ * Indicazioni di punteggio:
+ * - correttezza della parte obbligatoria: 10 punti
+ * - qualità della parte opzionale: 5 punti
+ * - compilazione/esecuzione da linea di comando: 2 punti
+ */
 public interface Logics {
     Optional<Integer> hit(int x, int y);
 
--- src/main/scala/it/unibo/pps/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a01b/LogicsImpl.scala
@@ -3,12 +3,47 @@ package it.unibo.pps.polyglot.a01b
 import it.unibo.pps.polyglot.OptionToOptional
 import it.unibo.pps.polyglot.a01b.Logics
 import it.unibo.pps.util.Optionals.Optional as ScalaOptional
+import it.unibo.pps.util.Sequences.*
+import Sequence.*
 
+import scala.annotation.tailrec
 import scala.jdk.javaapi.OptionConverters
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
-  def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    OptionToOptional(ScalaOptional.Empty()) // Option => Optional converter
+  private var cellsUntilWon = size * size - mines
+  private var lost = false
+  private var minePositions: Sequence[(Int, Int)] = Nil()
+  for i <- 1 to mines do {
+    minePositions = Cons(spawnMine, minePositions)
+  }
+  println(minePositions)
 
-  def won = false
+  @tailrec
+  private def spawnMine: (Int, Int) =
+    val random = scala.util.Random()
+    val newMine = (random.nextInt(size - 1), random.nextInt(size - 1))
+    if minePositions.contains(newMine) then spawnMine else newMine
+    
+  private def checkAdjacentCoordinate(possibleAdjacent: Int, target: Int): Boolean =
+    possibleAdjacent == target - 1 || possibleAdjacent == target || possibleAdjacent == target + 1
+  
+  @tailrec
+  private def calculateAdjacentMines(using cell: (x: Int, y: Int))(minesPos: Sequence[(Int, Int)], acc: Int): Int =
+    minesPos match
+      case Cons((mineX, mineY), tail) if checkAdjacentCoordinate(mineX, cell.x) && checkAdjacentCoordinate(mineY, cell.y) 
+        => calculateAdjacentMines(tail, acc + 1)
+      case Cons(_, tail) => calculateAdjacentMines(tail, acc)
+      case Nil() => acc
+
+  def hit(x: Int, y: Int): java.util.Optional[Integer] =
+    if (minePositions.contains((x, y)))
+      lost = true
+      OptionToOptional(ScalaOptional.Empty())
+    else
+      cellsUntilWon = cellsUntilWon - 1
+      given cell: (Int, Int) = (x, y)
+      OptionToOptional(ScalaOptional.Just(calculateAdjacentMines(minePositions, 0)))
+
+
+  override def won: Boolean = cellsUntilWon == 0 && !lost
